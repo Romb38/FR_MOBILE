c0a1052fcc8cd9da736c82739079f6e1745bdb2b
Romb38 <r.barbier38@gmail.com>
2025-03-19 17:32:32 +0100

    Ajouts de retour utilisateurs lors de l'ajout de reader/writer

--- src/app/add-reader-writer-modal/add-reader-writer-modal.component.ts
+++ src/app/add-reader-writer-modal/add-reader-writer-modal.component.ts
@@ -1,10 +1,12 @@
 import { NgIf } from '@angular/common';
 import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ToastController } from "@ionic/angular/standalone"
 import { IonModal, IonButton, IonHeader, IonToolbar,  IonButtons, IonTitle, IonContent, IonItem, IonInput, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 import { Topic } from '../models/topic';
 import { TopicService } from '../services/topic.service';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { firstValueFrom } from 'rxjs';
 
 
 @Component({
@@ -35,6 +37,8 @@ export class AddReaderWriterModalComponent  implements OnInit {
   @Output() close = new EventEmitter<void>();
   protected email : string = "";
   private topicService : TopicService = inject(TopicService)
+  private toastController = inject(ToastController)
+  translate = inject(TranslateService);
 
   constructor() { }
 
@@ -48,14 +52,60 @@ export class AddReaderWriterModalComponent  implements OnInit {
     this.close.emit(); 
   }
 
-  onAddRead(){
-    this.topicService.addTopicReader(this.topic,this.email)
-    this.close.emit()
+  async onAddRead(){
+    if (this.topic.readers.includes(this.email)){
+      const translatedMessage = await firstValueFrom(
+        this.translate.get('USER_ALREADY_READER')
+      );      
+      const toast = await this.toastController.create({
+        message: translatedMessage,
+        duration: 1500,
+        position: 'bottom',
+      });
+      await toast.present(); 
+    } else if (this.topic.editors.includes(this.email)){
+      const translatedMessage = await firstValueFrom(
+        this.translate.get('USER_ALREADY_WRITER')
+      );       
+      const toast = await this.toastController.create({
+        message: translatedMessage,
+        duration: 1500,
+        position: 'bottom',
+      });
+      await toast.present();
+    } else {
+      this.topicService.addTopicReader(this.topic,this.email)
+      this.close.emit()
+    }
   }
   
-  onAddWrite(){
-    this.topicService.addTopicWriter(this.topic,this.email)
-    this.close.emit()
+  async onAddWrite(){
+
+    if (this.topic.readers.includes(this.email)){
+      const translatedMessage = await firstValueFrom(
+        this.translate.get('USER_ALREADY_READER')
+      );       
+      const toast = await this.toastController.create({
+        message: translatedMessage,
+        duration: 1500,
+        position: 'bottom',
+      });
+      await toast.present();
+    } else if (this.topic.editors.includes(this.email)){
+      const translatedMessage = await firstValueFrom(
+        this.translate.get('USER_ALREADY_WRITER')
+      );       
+      const toast = await this.toastController.create({
+        message: translatedMessage,
+        duration: 1500,
+        position: 'bottom',
+      });
+      await toast.present();
+    } else {
+      this.topicService.addTopicWriter(this.topic,this.email)
+      this.close.emit() 
+    }
+    
   }
 
 
--- src/app/edit-reader-writer-modal/edit-reader-writer-modal.component.ts
+++ src/app/edit-reader-writer-modal/edit-reader-writer-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { Component, computed, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { Topic } from '../models/topic';
 import {IonModal, IonButton, IonHeader, IonToolbar,  IonButtons, IonTitle, IonContent, IonList, IonItem, IonLabel, IonIcon} from '@ionic/angular/standalone'
 import { TopicService } from '../services/topic.service';
@@ -11,22 +11,19 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrls: ['./edit-reader-writer-modal.component.scss'],
   imports: [IonModal, IonHeader, IonToolbar, IonButton, IonButtons, IonTitle, IonContent, IonList, IonItem, IonLabel, IonIcon, TranslateModule]
 })
-export class EditReaderWriterModalComponent  implements OnInit {
+export class EditReaderWriterModalComponent {
 
   @Input() isVisible: boolean = false;
   @Input() topic: Topic = {} as Topic
   @Output() close = new EventEmitter<void>();
-  protected noReader : boolean = true
-  protected noWriter : boolean = true
+  protected noReader = computed(() => !(this.topic.readers?.length > 0));
+  protected noWriter = computed(() => !(this.topic.readers?.length > 0));
   private topicService : TopicService = inject(TopicService)
 
-  constructor() { }
 
-  ngOnInit() {
-    this.noReader = !(this.topic.readers.length > 0);
-    this.noWriter = !(this.topic.editors.length > 0)
+  constructor() { 
+    console.log(this.topic)
   }
-
 
   closeModal(): void {
     this.close.emit();
