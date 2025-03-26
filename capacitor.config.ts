70aa6c8a7ca74a808a310f7d4f0a4aa5f315870b
florent-dev <florent84@live.fr>
2025-03-26 15:20:11 +0100

    update: package_name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'fr.standouda.mobile',
+  appId: 'com.m2gi.mobileapp',
   appName: 'Reddot',
   webDir: 'www',
 };
